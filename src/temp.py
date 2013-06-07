0758870c715285f8b08f268245588f5a1359edbc
Will Parkin <wmparkin@gmail.com>
2013-06-06 20:14:05 -0400

    hmm

--- src/temp.py
+++ src/temp.py
@@ -15,11 +15,13 @@ Do everything for the first dataset
 # load the matlab file with parameters for the runs
 specsfile = sio.loadmat('/home/parkin/Documents/DrndicLab/data/sample/all.mat')
 
-print specsfile
+print specsfile.keys()
 
 struct = specsfile['All']
 
 print struct.shape
+print struct[0,1]
+print struct[0,1]['event_data']
 first = struct[0,1]
 event = first['event_data'][0][0]
 time = np.linspace(first['start_time'][0][0][0][0], first['end_time'][0][0][0][0], len(event))
