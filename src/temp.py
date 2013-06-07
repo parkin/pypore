463858b48287cedf81eb35a5b88bb04833f82c6a
Will Parkin <wmparkin@gmail.com>
2013-06-07 17:42:13 -0400

    save Events file after analyzing data

--- src/temp.py
+++ src/temp.py
@@ -7,24 +7,27 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import numpy as np
 
-specsfile = sio.loadmat('/home/parkin/Documents/DrndicLab/data/sample/events.mat')
+# specsfile = sio.loadmat('/home/parkin/Documents/DrndicLab/data/sample/events.mat')
 
 '''
 Do everything for the first dataset
 '''
 # load the matlab file with parameters for the runs
-specsfile = sio.loadmat('/home/parkin/Documents/DrndicLab/data/sample/all.mat')
+# specsfile = sio.loadmat('/home/parkin/Documents/DrndicLab/data/sample/all.mat')
+specsfile = sio.loadmat('/home/parkin/Eclipse/drndiclabworkspace/com.parkin.python.drniclab/data/polyA_20121221_142654_Events.mat')
 
 print specsfile.keys()
 
-struct = specsfile['All']
+events = specsfile['Events']
 
-print struct.shape
-print struct[0,1]
-print struct[0,1]['event_data']
-first = struct[0,1]
+print events.shape
+print specsfile['sample_rate']
+print events[0]
+first = events[4,0]
 event = first['event_data'][0][0]
-time = np.linspace(first['start_time'][0][0][0][0], first['end_time'][0][0][0][0], len(event))
+# time = np.linspace(first['start_time'][0][0][0][0], first['end_time'][0][0][0][0], len(event))
+time = np.linspace(0, len(event)/float(specsfile['sample_rate']), len(event))
+print time
 
 print len(time),len(event)
 
