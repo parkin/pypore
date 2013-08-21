7a01865e57d95ddd4a83e7d539c27a06cceb1664
Will Parkin <wmparkin@gmail.com>
2013-08-20 21:08:40 -0400

    added test class for views, needs implementation

---
+++ src/pyporegui/test/TestViews.py
@@ -0,0 +1,27 @@
+'''
+Created on Aug 20, 2013
+
+@author: parkin
+'''
+import unittest
+from pyporegui.views import *
+
+
+class TestViews(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
