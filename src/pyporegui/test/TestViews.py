7bd5cb12621dd4a2622f3ac943568d01972de9b3
Will Parkin <wmparkin@gmail.com>
2013-08-21 11:12:28 -0400

    added test for color. moved to pyside imports

--- src/pyporegui/test/TestViews.py
+++ src/pyporegui/test/TestViews.py
@@ -4,12 +4,11 @@ Created on Aug 20, 2013
 @author: parkin
 '''
 import unittest
-from pyporegui.views import *
 import os
-
+from src.pyporegui.views import FilterListItem
+from PySide.QtGui import QColor
 
 class TestViews(unittest.TestCase):
-
 
     def setUp(self):
         pass
@@ -18,15 +17,21 @@ class TestViews(unittest.TestCase):
     def tearDown(self):
         pass
 
-
     def testFilterListItemSimpleName(self):
         filenames = [os.path.abspath(__file__)]
-        params = {}
-        item = FilterListItem(filenames, params)
+        item = FilterListItem(filenames)
         simplenames = item.getSimpleNames()
         self.assertEqual(len(simplenames), len(filenames))
         self.assertEqual(simplenames[0], 'TestViews.py')
         self.assertEqual(item.getSimpleNameAt(0), 'TestViews.py')
+        
+    def testFilterListItemDefaultColor(self):
+        filenames = ['hi.txt']
+        item = FilterListItem(filenames)
+        color = item.getParams()['color']
+        self.assertTrue(type(color) is QColor)
+        
+    
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
