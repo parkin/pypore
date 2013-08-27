872e89ec7af322f692357a394a9eaa99787ce924
Will Parkin <wmparkin@gmail.com>
2013-08-27 15:55:22 -0400

    added unit test module for EventFinder

---
+++ src/pypore/test/TestEventFinder.py
@@ -0,0 +1,25 @@
+'''
+Created on Aug 27, 2013
+
+@author: parkin
+'''
+import unittest
+
+
+class TestEventFinder(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
