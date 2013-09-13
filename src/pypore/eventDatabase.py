5abd641e8c3f5a299d01169bc428ddd1b0405b63
Will Parkin <wmparkin@gmail.com>
2013-09-13 09:48:05 -0400

    implemented initializing an event database

--- src/pypore/eventDatabase.py
+++ src/pypore/eventDatabase.py
@@ -7,8 +7,29 @@ Created on Sep 13, 2013
 import tables as tb
 
 # Description of events table
-class Events(tb.IsDescription):
-    arrayRow = tb.UInt32Atom()      # indicates the corresponding row in the
+class Event(tb.IsDescription):
+    # UIntAtom = uint32
+    arrayRow = tb.UIntCol(pos=0)      # indicates the corresponding row in the
                                     # eventData and rawData etc VLArrays
-    eventStart = tb.UInt64Atom()    # start index of the event in the data
+    eventStart = tb.UIntCol(itemsize=8, pos=1)    # start index of the event in the data
+    eventLength = tb.UIntCol(pos=2)
+    rawPointsPerSide = tb.UIntCol(pos=3)
+    baseline = tb.FloatCol(pos=4)
+    currentBlockage = tb.FloatCol(pos=5)
+    area = tb.FloatCol(pos=6)
     
+def initializeEventsDatabase(filename):
+    h5file = tb.openFile(filename, mode='w')
+    h5file.createGroup(h5file.root, 'events', 'Events')
+    h5file.createTable(h5file.root.events, 'eventTable', Event, 'Event parameters')
+    h5file.createVLArray(h5file.root.events, 'rawData',
+                         tb.FloatAtom(shape=()),
+                        "Raw data with raw points buffer.")
+    h5file.createVLArray(h5file.root.events, 'levels',
+                         tb.FloatAtom(shape=()),
+                        "Cusum levels")
+    h5file.createVLArray(h5file.root.events, 'levelIndices',
+                         tb.FloatAtom(shape=()),
+                        "Indices for cusum levels")
+    
+    return h5file
