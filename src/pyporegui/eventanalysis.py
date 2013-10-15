ce72cedc85ebfa52b87f143676a00aaff0a71a07
Will Parkin <wmparkin@gmail.com>
2013-10-15 15:40:04 -0400

    plugged in removing event analysis item

--- src/pyporegui/eventanalysis.py
+++ src/pyporegui/eventanalysis.py
@@ -24,7 +24,7 @@ def _longImports(**kwargs):
     global AnalyzeDataThread, PlotThread, FileListItem, FilterListItem, \
             PlotToolBar, DataFileListItem, MyPlotItem, prepareDataFile, pg, pgc, \
             LayoutWidget, PlotCurveItem, linspace, np, tb, \
-            MySpotItem, MyScatterPlotItem
+            MySpotItem, MyScatterPlotItem, EventAnalysisWidget
         
     updateSplash = False    
     if 'splash' in kwargs and 'app' in kwargs:
@@ -66,7 +66,7 @@ def _longImports(**kwargs):
         app.processEvents()
     from MyThreads import AnalyzeDataThread, PlotThread
     from views import FileListItem, FilterListItem, PlotToolBar, DataFileListItem, MyPlotItem
-    from views import MySpotItem, MyScatterPlotItem
+    from views import MySpotItem, MyScatterPlotItem, EventAnalysisWidget
     
     if updateSplash: 
         splash.showMessage("Importing PyTables", alignment=QtCore.Qt.AlignBottom)
@@ -101,7 +101,6 @@ class MyMainWindow(QtGui.QMainWindow):
         self.openDir = '../../data'
         
         self.threadPool = []
-        self.lastScatterClicked = []
         
         self.setWindowTitle('Translocation Event Analysis')
         
@@ -365,7 +364,8 @@ class MyMainWindow(QtGui.QMainWindow):
         vbox = QtGui.QVBoxLayout()
         vbox.addLayout(formFilter)
         vbox.addWidget(self.listFilterWidget)
-        btnRemoveFilter = QtGui.QPushButton('Remove selected filter')
+        btnRemoveFilter = QtGui.QPushButton('Remove selected filters')
+        btnRemoveFilter.clicked.connect(self.removeFilterClicked)
         vbox.addWidget(btnRemoveFilter)
         
         vbox_left = QtGui.QVBoxLayout()
@@ -394,7 +394,14 @@ class MyMainWindow(QtGui.QMainWindow):
         item = FilterListItem(filenames, **params)
         self.listFilterWidget.addItem(item)
         
-        self.plotEventDatabaseAnalyses(filenames, params)
+        self.eventAnalysisWidget.addSelections(filenames, params)
+        
+    def removeFilterClicked(self):
+        items = self.listFilterWidget.selectedItems()
+        for item in items:
+            index = self.listFilterWidget.indexFromItem(item).row()
+            self.eventAnalysisWidget.removeFilter(index)
+            self.listFilterWidget.takeItem(index)
         
     def _getCurrentEventAnalysisParams(self):
         params = {}
@@ -464,20 +471,9 @@ class MyMainWindow(QtGui.QMainWindow):
     def _create_eventanalysis_plot_widget(self):
         # Tab widget for event stuff
         
-        vwig = pg.GraphicsLayoutWidget()
-        self.plot_eventdepth = vwig.addPlot(title='Event Depth')
-        self.plot_eventdepth.setMouseEnabled(x=False, y=True)
-        self.plot_eventdur_eventdepth = vwig.addPlot(name='Depth vs. Duration', title='Depth vs. Duration')
-        self.plot_eventdepth.setYLink('Depth vs. Duration')
+        self.eventAnalysisWidget = EventAnalysisWidget()
         
-        vwig.nextRow()
-        
-        self.plot_scatterselect = vwig.addPlot(title='Single Event')
-        self.plot_eventdur = vwig.addPlot(title='Event Duration')
-        self.plot_eventdur.setXLink('Depth vs. Duration')
-        self.plot_eventdur.setMouseEnabled(x=True, y=False)
-        
-        return vwig
+        return self.eventAnalysisWidget
     
     def _create_event_analysis_tab(self):
         frame = QtGui.QSplitter()
@@ -641,127 +637,6 @@ The current namespace should include:
         if checkable:
             action.setCheckable(True)
         return action
-    
-    def onScatterPointsClicked(self, plot, points):
-        """
-        Callback for when a scatter plot points are clicked.
-        Highlights the points and unhighlights previously selected points.
-        
-        plot should be a MyScatterPlotItem
-        points should be a MySpotItem
-        """
-        for p in self.lastScatterClicked:
-            p.resetPen()
-            # remove point we've already selected so we
-            # can select points behind it.
-            if p in points and len(points) > 1:
-                points.remove(p)
-#         print 'Points clicked:', points, plot
-        for point in points:
-            point.setPen('w', width=2)
-            self.lastScatterClicked = [point]
-            break # only take first point
-        
-        # Plot the new point clicked on the single event display
-        filename, position = plot.getFileNameFromPosition(self.lastScatterClicked[0].eventPosition)
-        
-        h5file = tb.openFile(filename, mode='r')
-        
-        table = h5file.root.events.eventTable
-        row = table[position]
-        arrayRow = row['arrayRow']
-        sampleRate = table.attrs.sampleRate
-        eventLength = row['eventLength']
-        rawPointsPerSide = row['rawPointsPerSide']
-        n = eventLength+2*rawPointsPerSide
-        
-        rawData = h5file.root.events.rawData[arrayRow][:n]
-        
-        times = np.linspace(0.0, 1.0*n/sampleRate, n)
-        
-        self.plot_scatterselect.clear()
-        self.plot_scatterselect.plot(times, rawData)
-        # plot the event points in yellow
-        self.plot_scatterselect.plot(times[rawPointsPerSide:rawPointsPerSide+eventLength],\
-                                     rawData[rawPointsPerSide:rawPointsPerSide+eventLength], pen='y')
-        
-        # Plot the cusum levels
-        nLevels = row['nLevels']
-        baseline = row['baseline']
-        eventStart = row['eventStart']
-        # left, start-1, start, 
-        levels = h5file.root.events.levels[arrayRow][:nLevels]
-        indices = h5file.root.events.levelIndices[arrayRow][:nLevels+1]
-        indices -= eventStart
-        
-        levelTimes = np.zeros(2*nLevels+4)
-        levelValues = np.zeros(2*nLevels+4)
-        
-        levelTimes[1] = 1.0*(rawPointsPerSide-1)/sampleRate
-        levelValues[0] = levelValues[1] = baseline
-        i = 0
-        for i in xrange(nLevels):
-            levelTimes[2*i+2] = times[rawPointsPerSide] + 1.0*(indices[i])/sampleRate
-            levelValues[2*i+2] = levels[i]
-            if i < nLevels:
-                levelTimes[2*i+3] = times[rawPointsPerSide] + 1.0*(indices[i+1])/sampleRate
-                levelValues[2*i+3] = levels[i]
-        i += 1        
-        levelTimes[2*i+2] = times[rawPointsPerSide+eventLength]
-        levelTimes[2*i+3] = times[n-1]
-        levelValues[2*i+2] = levelValues[2*i+3] = baseline
-        
-        self.plot_scatterselect.plot(levelTimes, levelValues, pen='g')
-        
-        h5file.close()
-    
-    def plotEventDatabaseAnalyses(self, filenames, params):
-        '''
-        Plots event statistics.  
-        '''
-        files = []
-        counts = []
-        eventCount = 0
-        for filename in filenames:
-            h5file = tb.openFile(filename, mode='r')
-            files.append(h5file)
-            count = h5file.root.events.eventTable.attrs.eventCount
-            eventCount += count
-            counts.append(count)
-        
-        currentBlockade = np.empty(eventCount)
-        dwellTimes = np.empty(eventCount)
-        count = 0
-        for j, filex in enumerate(files):
-            eventTable = filex.root.events.eventTable
-            sample_rate = filex.root.events.eventTable.attrs.sampleRate
-            for i, row in enumerate(eventTable):
-                currentBlockade[count+i] = row['currentBlockage']
-                dwellTimes[count+i] = row['eventLength'] / sample_rate
-            count += counts[j]
-                
-        color = params['color']
-        newcolor = QtGui.QColor(color.red(), color.green(), color.blue(), 128)
-                 
-        bins = eventCount ** 0.5
-        y_dt, x_dt = np.histogram(dwellTimes, bins=bins)        
-        curve_dt = PlotCurveItem(x_dt, y_dt, stepMode=True, fillLevel=0, brush=newcolor)
-        self.plot_eventdur.addItem(curve_dt)
-        
-        y_cb, x_cb = np.histogram(currentBlockade, bins=bins)        
-        curve_cb = PlotCurveItem(-1.*x_cb, y_cb, stepMode=True, fillLevel=0, brush=newcolor)
-        curve_cb.rotate(-90)
-        self.plot_eventdepth.addItem(curve_cb)
-        
-        scatterItem = MyScatterPlotItem(size=10, pen=pg.mkPen(None), brush=newcolor, files=filenames, counts=counts)
-        scatterItem.setData(dwellTimes, currentBlockade)
-        self.plot_eventdur_eventdepth.addItem(scatterItem)
-        scatterItem.sigClicked.connect(self.onScatterPointsClicked)
-        
-        for filex in files:
-            filex.close()
-        
-        return
     
     def plotData(self, plot_options):
         '''
--- src/pyporegui/views.py
+++ src/pyporegui/views.py
@@ -5,8 +5,160 @@ Created on Aug 6, 2013
 '''
 from PySide import QtGui
 import os.path
+
+import tables as tb
+import numpy as np
+
 from pyqtgraph.graphicsItems.PlotItem.PlotItem import PlotItem
 from pyqtgraph.graphicsItems.ScatterPlotItem import ScatterPlotItem, SpotItem
+from pyqtgraph.widgets.GraphicsLayoutWidget import GraphicsLayoutWidget
+from pyqtgraph.graphicsItems.PlotCurveItem import PlotCurveItem
+from pyqtgraph.functions import mkPen
+
+class EventAnalysisWidget(GraphicsLayoutWidget):
+    pass
+
+    def __init__(self):
+        super(EventAnalysisWidget, self).__init__()
+        
+        self.plot_eventdepth = self.addPlot(title='Event Depth')
+        self.plot_eventdepth.setMouseEnabled(x=False, y=True)
+        self.plot_eventdur_eventdepth = self.addPlot(name='Depth vs. Duration', title='Depth vs. Duration')
+        self.plot_eventdepth.setYLink('Depth vs. Duration')
+        
+        self.nextRow()
+        
+        self.plot_scatterselect = self.addPlot(title='Single Event')
+        self.plot_eventdur = self.addPlot(title='Event Duration')
+        self.plot_eventdur.setXLink('Depth vs. Duration')
+        self.plot_eventdur.setMouseEnabled(x=True, y=False)
+        
+        self.lastScatterClicked = []
+        
+    def addSelections(self, filenames, params):
+        '''
+        Plots event statistics.  
+        '''
+        files = []
+        counts = []
+        eventCount = 0
+        for filename in filenames:
+            h5file = tb.openFile(filename, mode='r')
+            files.append(h5file)
+            count = h5file.root.events.eventTable.attrs.eventCount
+            eventCount += count
+            counts.append(count)
+        
+        currentBlockade = np.empty(eventCount)
+        dwellTimes = np.empty(eventCount)
+        count = 0
+        for j, filex in enumerate(files):
+            eventTable = filex.root.events.eventTable
+            sample_rate = filex.root.events.eventTable.attrs.sampleRate
+            for i, row in enumerate(eventTable):
+                currentBlockade[count+i] = row['currentBlockage']
+                dwellTimes[count+i] = row['eventLength'] / sample_rate
+            count += counts[j]
+                
+        color = params['color']
+        newcolor = QtGui.QColor(color.red(), color.green(), color.blue(), 128)
+                 
+        bins = eventCount ** 0.5
+        y_dt, x_dt = np.histogram(dwellTimes, bins=bins)        
+        curve_dt = PlotCurveItem(x_dt, y_dt, stepMode=True, fillLevel=0, brush=newcolor)
+        self.plot_eventdur.addItem(curve_dt)
+        
+        y_cb, x_cb = np.histogram(currentBlockade, bins=bins)        
+        curve_cb = PlotCurveItem(-1.*x_cb, y_cb, stepMode=True, fillLevel=0, brush=newcolor)
+        curve_cb.rotate(-90)
+        self.plot_eventdepth.addItem(curve_cb)
+        scatterItem = MyScatterPlotItem(size=10, pen=mkPen(None), brush=newcolor, files=filenames, counts=counts)
+        scatterItem.setData(dwellTimes, currentBlockade)
+        self.plot_eventdur_eventdepth.addItem(scatterItem)
+        scatterItem.sigClicked.connect(self.onScatterPointsClicked)
+        
+        for filex in files:
+            filex.close()
+        
+        return
+    
+    def removeFilter(self, index):
+        self.plot_eventdur.removeItem(self.plot_eventdur.listDataItems()[index])
+        self.plot_eventdepth.removeItem(self.plot_eventdepth.listDataItems()[index])
+        self.plot_eventdur_eventdepth.removeItem(self.plot_eventdur_eventdepth.listDataItems()[index])
+    
+    def onScatterPointsClicked(self, plot, points):
+        """
+        Callback for when a scatter plot points are clicked.
+        Highlights the points and unhighlights previously selected points.
+        
+        plot should be a MyScatterPlotItem
+        points should be a MySpotItem
+        """
+        for p in self.lastScatterClicked:
+            p.resetPen()
+            # remove point we've already selected so we
+            # can select points behind it.
+            if p in points and len(points) > 1:
+                points.remove(p)
+#         print 'Points clicked:', points, plot
+        for point in points:
+            point.setPen('w', width=2)
+            self.lastScatterClicked = [point]
+            break # only take first point
+        
+        # Plot the new point clicked on the single event display
+        filename, position = plot.getFileNameFromPosition(self.lastScatterClicked[0].eventPosition)
+        
+        h5file = tb.openFile(filename, mode='r')
+        
+        table = h5file.root.events.eventTable
+        row = table[position]
+        arrayRow = row['arrayRow']
+        sampleRate = table.attrs.sampleRate
+        eventLength = row['eventLength']
+        rawPointsPerSide = row['rawPointsPerSide']
+        n = eventLength+2*rawPointsPerSide
+        
+        rawData = h5file.root.events.rawData[arrayRow][:n]
+        
+        times = np.linspace(0.0, 1.0*n/sampleRate, n)
+        
+        self.plot_scatterselect.clear()
+        self.plot_scatterselect.plot(times, rawData)
+        # plot the event points in yellow
+        self.plot_scatterselect.plot(times[rawPointsPerSide:rawPointsPerSide+eventLength],\
+                                     rawData[rawPointsPerSide:rawPointsPerSide+eventLength], pen='y')
+        
+        # Plot the cusum levels
+        nLevels = row['nLevels']
+        baseline = row['baseline']
+        eventStart = row['eventStart']
+        # left, start-1, start, 
+        levels = h5file.root.events.levels[arrayRow][:nLevels]
+        indices = h5file.root.events.levelIndices[arrayRow][:nLevels+1]
+        indices -= eventStart
+        
+        levelTimes = np.zeros(2*nLevels+4)
+        levelValues = np.zeros(2*nLevels+4)
+        
+        levelTimes[1] = 1.0*(rawPointsPerSide-1)/sampleRate
+        levelValues[0] = levelValues[1] = baseline
+        i = 0
+        for i in xrange(nLevels):
+            levelTimes[2*i+2] = times[rawPointsPerSide] + 1.0*(indices[i])/sampleRate
+            levelValues[2*i+2] = levels[i]
+            if i < nLevels:
+                levelTimes[2*i+3] = times[rawPointsPerSide] + 1.0*(indices[i+1])/sampleRate
+                levelValues[2*i+3] = levels[i]
+        i += 1        
+        levelTimes[2*i+2] = times[rawPointsPerSide+eventLength]
+        levelTimes[2*i+3] = times[n-1]
+        levelValues[2*i+2] = levelValues[2*i+3] = baseline
+        
+        self.plot_scatterselect.plot(levelTimes, levelValues, pen='g')
+        
+        h5file.close()
 
 class MySpotItem(SpotItem):
     
