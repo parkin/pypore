b18677124057bb6dcec79adddb0de08411e4d6a8
parkin1 <wmparkin@gmail.com>
2014-02-21 04:14:28 -0500

    Website at Fri Feb 21 04:14:27 EST 2014

--- sphinx-docs/source/conf.py
+++ sphinx-docs/source/conf.py
@@ -41,8 +41,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'translocationcode-python'
-copyright = u'2013, Will Parkin'
+project = u'pypore'
+copyright = u'2014, Will Parkin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -93,7 +93,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'sphinxdoc'
+html_theme = 'architect'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -101,7 +101,7 @@ html_theme = 'sphinxdoc'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ["../themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
