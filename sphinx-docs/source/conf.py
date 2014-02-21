ce5e6411bd1190f1f5c5bb9faedf2bc89ce3cd53
parkin1 <wmparkin@gmail.com>
2014-02-21 05:14:43 -0500

    Website at Fri Feb 21 05:14:43 EST 2014

--- sphinx-docs/source/conf.py
+++ sphinx-docs/source/conf.py
@@ -215,7 +215,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'translocationcode-python', u'translocationcode-python Documentation',
+    ('index', 'pypore', u'pypore Documentation',
      [u'Will Parkin'], 1)
 ]
 
@@ -229,8 +229,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'translocationcode-python', u'translocationcode-python Documentation',
-   u'Will Parkin', 'translocationcode-python', 'One line description of project.',
+  ('index', 'pypore', u'translocationcode-python Documentation',
+   u'Will Parkin', 'pyore', 'One line description of project.',
    'Miscellaneous'),
 ]
 
