42028240400e3c55eb18ba4fd43eab8d60c6f720
Will <wmparkin@gmail.com>
2014-03-05 16:08:43 -0500

    added test for pre/post array equality

--- src/pypore/test/TestFileConverter.py
+++ src/pypore/test/TestFileConverter.py
@@ -5,8 +5,11 @@
 import unittest
 import os
 from pypore.dataFile import DataFile
+from pypore.dataFileOpener import open_data
 
 from pypore.fileConverter import convert_file
+
+import numpy as np
 
 
 class TestFileConverter(unittest.TestCase):
@@ -21,29 +24,47 @@ class TestFileConverter(unittest.TestCase):
         filename = os.path.dirname(os.path.realpath(__file__))
         filename = os.path.join(filename, 'testDataFiles', 'chimera_1event.log')
 
-        output_filename = "out.h5"
+        output_filename_set = "out.h5"
 
-        output_file = convert_file(filename, output_filename=output_filename)
+        output_filename = convert_file(filename, output_filename=output_filename_set)
 
         # Test that we can set the output filename
-        self.assertEqual(output_file, output_filename, "output_filename not correct")
+        self.assertEqual(output_filename, output_filename_set, "output_filename not correct")
 
-        os.remove(output_file)
+        os.remove(output_filename)
 
     def test_convert_file_matrix_equality(self):
         filename = os.path.dirname(os.path.realpath(__file__))
         filename = os.path.join(filename, 'testDataFiles', 'chimera_1event.log')
 
-        output_filename = "out.h5"
+        output_filename_set = "out.h5"
 
-        output_file = convert_file(filename, output_filename=output_filename)
+        output_filename = convert_file(filename, output_filename=output_filename_set)
 
-        # open the output_file
-        data_file = DataFile(output_file)
+        orig = open_data(filename)
+        orig_datas = orig['data']
+        orig_sample_rate = orig['sample_rate']
 
+        self.assertEqual(len(orig_datas), 1)
 
+        out = open_data(output_filename)
+        out_datas = out['data']
+        out_sample_rate = out['sample_rate']
 
-        pass
+        self.assertEqual(len(out_datas), 1)
+
+        orig_data = orig_datas[0]
+        out_data = out_datas[0]
+
+        # assert sample rates are equal
+        threshold = -1*int(np.log10(orig_sample_rate)) + 4
+        print threshold
+        self.assertAlmostEqual(orig_sample_rate, out_sample_rate, threshold)
+
+        # assert the two arrays are equal
+        np.testing.assert_array_equal(orig_data, out_data)
+
+        os.remove(output_filename)
 
 
 if __name__ == "__main__":
