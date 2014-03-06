f7cd2e75274b3227639a90574816e5b46086f5fa
Will <wmparkin@gmail.com>
2014-03-06 12:56:22 -0500

    removed unused methods. more PEP8 stuff

--- src/pyporegui/gui.py
+++ src/pyporegui/gui.py
@@ -24,7 +24,7 @@ def _long_imports(**kwargs):
         sys.path.append(src_dir)
 
     global AnalyzeDataThread, PlotThread, pg, pgc, LayoutWidget, linspace, np, \
-            EventAnalysisWidget, ed, EventFindingTab, EventViewingTab, EventAnalysisTab
+        EventAnalysisWidget, ed, EventFindingTab, EventViewingTab, EventAnalysisTab
 
     update_splash = False
     if 'splash' in kwargs and 'app' in kwargs:
@@ -88,9 +88,9 @@ class MyMainWindow(QtGui.QMainWindow):
 
         self.setWindowTitle('Translocation Event Analysis')
 
-        self.create_menu()
+        self._create_menu()
         self._create_main_frame()
-        self.create_status_bar()
+        self._create_status_bar()
 
     def open_files(self):
         """
@@ -173,14 +173,14 @@ The current namespace should include:
 
         self.setCentralWidget(frame)
 
-    def create_status_bar(self):
+    def _create_status_bar(self):
         """
         Creates filter_parameter status bar with filter_parameter text widget.
         """
         self.status_text = QtGui.QLabel("")
         self.statusBar().addWidget(self.status_text, 1)
 
-    def create_menu(self):
+    def _create_menu(self):
         """
         Creates File menu with Open
         """
@@ -205,7 +205,8 @@ The current namespace should include:
     #
     #         self.add_actions(self.help_menu, (about_action,))
 
-    def add_actions(self, target, actions):
+    @classmethod
+    def add_actions(cls, target, actions):
         for action in actions:
             if action is None:
                 target.addSeparator()
@@ -215,6 +216,10 @@ The current namespace should include:
     def create_action(self, text, slot=None, shortcut=None,
                       icon=None, tip=None, checkable=False,
                       signal="triggered()"):
+        """
+        Create a :py:class:`QtGui.QAction`.
+        :param text: Text to show in the file menu
+        """
         action = QtGui.QAction(text, self)
         if icon is not None:
             action.setIcon(QtGui.QIcon(":/%s.png" % icon))
@@ -229,185 +234,6 @@ The current namespace should include:
             action.setCheckable(True)
         return action
 
-    def plotData(self, plot_options):
-        '''
-        Plots waveform in datadict
-        Pass in plot_options, filter_parameter dictionary with 'plot_range', 'axes', and 'datadict'
-        pass in Data dictionary, with data at 'data' and sample rate at 'SETUP_ADCSAMPLERATE'
-        Can pass in range as [start,stop], or 'all' for 0:n
-        '''
-        axes = plot_options['axes']
-        if axes is None:
-            axes = self.plot
-        # Read the first file, store data in dictionary
-        data = plot_options['datadict']['data'][0]
-        sample_rate = plot_options['datadict']['sample_rate']
-        plot_range = plot_options['plot_range']
-
-        n = len(data)
-        # If problem with input, just plot all the data
-        if plot_range == 'all' or len(plot_range) != 2 or plot_range[1] <= plot_range[0]:
-            plot_range = [0, n]
-        else:  # no problems!
-            n = plot_range[1] - plot_range[0] + 1
-
-        Ts = 1 / sample_rate
-
-        times = linspace(Ts * plot_range[0], Ts * plot_range[1], n)
-        yData = data[plot_range[0]:(plot_range[1] + 1)]
-
-        self.plotwid.clear_event_items()
-        self.p1.setData(x=times, y=yData)
-        self.plotwid.autoRange()
-        self.app.processEvents()
-
-    # def addEventsToConcatEventPlot(self, events):
-    #     if len(events) < 1:
-    #         return
-    #     size = 0
-    #     for event in events:
-    #         size += event['raw_data'].size
-    #     data = np.empty(size)
-    #     sample_rate = events[0]['sample_rate']
-    #     ts = 1 / sample_rate
-    #     times = np.linspace(0., (size - 1) * ts, size) + self.prev_concat_time
-    #     self.prev_concat_time += size * ts
-    #     index = 0
-    #     for event in events:
-    #         d = event['raw_data'].size
-    #         baseline = event['baseline']
-    #         data[index:index + d] = (event['raw_data'] - baseline)
-    #         index += d
-    #
-    #     item = PathItem(times, data)
-    #     item.setPen(pg.mkPen('w'))
-    #     self.plot_concatevents.addItem(item)
-
-    def getEventAndLevelsData(self, event):
-        data = event['raw_data']
-        levels_index = event['cusum_indexes']
-        levels_values = event['cusum_values']
-        sample_rate = event['sample_rate']
-        event_start = event['event_start']
-        event_end = event['event_end']
-        baseline = event['baseline']
-        raw_points_per_side = event['raw_points_per_side']
-
-        Ts = 1 / sample_rate
-
-        n = data.size
-
-        times = linspace(Ts * (event_start - raw_points_per_side), Ts * (event_start - raw_points_per_side + n - 1), n)
-        times2 = [(event_start - raw_points_per_side) * Ts, (event_start - 1) * Ts]
-        levels2 = [baseline, baseline]
-        for i, level_value in enumerate(levels_values):
-            times2.append(levels_index[i] * Ts)
-            levels2.append(level_value)
-            if i < len(levels_values) - 1:
-                times2.append((levels_index[i + 1] - 1) * Ts)
-                levels2.append(level_value)
-        times2.append(event_end * Ts)
-        levels2.append(levels_values[len(levels_values) - 1])
-        times2.append((event_end + 1) * Ts)
-        levels2.append(baseline)
-        times2.append((event_end + raw_points_per_side) * Ts)
-        levels2.append(baseline)
-        return times, data, times2, levels2
-
-    # def plotEventsOnMainPlot(self, events):
-    #     if len(events) < 1:
-    #         return
-    #     size = 0
-    #     for event in events:
-    #         size += event['raw_data'].size
-    #     data = np.empty(size)
-    #     sample_rate = events[0]['sample_rate']
-    #     raw_points_per_side = events[0]['raw_points_per_side']
-    #     ts = 1 / sample_rate
-    #     times = np.empty(size)
-    #     conn = np.ones(size)
-    #     index = 0
-    #     for event in events:
-    #         event_start = event['event_start']
-    #         event_data = event['raw_data']
-    #         d = event_data.size
-    #         times[index:index + d] = linspace(ts * (event_start - raw_points_per_side),
-    #                                           ts * (event_start - raw_points_per_side + d - 1), d)
-    #         data[index:index + d] = event_data
-    #         conn[index - 1] = False  # disconnect separate events
-    #         index += d
-    #
-    #     item = PathItem(times, data, conn)
-    #     item.setPen(pg.mkPen('y'))
-    #     self.plotwid.add_event_item(item)
-
-    def get_current_analysis_parameters(self):
-        '''
-        Returns filter_parameter dictionary holding the current analysis parameters set by the user.  Returns an entry 'error' if there were
-        invalid inputs.
-        '''
-        parameters = {}
-        # Get Min_event length in microseconds
-        try:
-            parameters['min_event_length'] = float(self.min_event_length_edit.text())
-        except ValueError:
-            parameters['error'] = 'Could not read float from Min Event Length text box.  Please fix.'
-            return parameters
-        # Get Max Event Length in microseconds
-        try:
-            parameters['max_event_length'] = float(self.max_event_length_edit.text())
-        except ValueError:
-            parameters['error'] = 'Could not read float from Max Event Length text box.  Please fix.'
-            return parameters
-        if parameters['min_event_length'] >= parameters['max_event_length']:
-            parameters['max_event_length'] = 'Min Event Length is greater than Max Event Length.  Please fix.'
-            return parameters
-
-        parameters['baseline_type'] = str(self.baseline_type_combo.currentText())
-        if parameters['baseline_type'] == 'Adaptive':
-            try:
-                parameters['filter_parameter'] = float(self.filter_parameter_edit.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Filter Parameter text box.  Please fix.'
-                return
-        elif parameters['baseline_type'] == 'Fixed':
-            try:
-                parameters['baseline_current'] = float(self.baseline_current_edit.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Baseline Current text box.  Please fix.'
-
-        parameters['threshold_direction'] = str(self.threshold_direction_combo.currentText())
-        parameters['threshold_type'] = str(self.threshold_type_combo.currentText())
-        if parameters['threshold_type'] == 'Noise Based':
-            try:
-                parameters['start_stddev'] = float(self.threshold_stdev_start.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Start StdDev text box.  Please fix.'
-            try:
-                parameters['end_stddev'] = float(self.threshold_stdev_end.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from End StdDev text box.  Please fix.'
-        elif parameters['threshold_type'] == 'Absolute Change':
-            try:
-                parameters['absolute_change_start'] = float(self.absolute_change_start_edit.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Absolute Change Start.  Please fix.'
-            try:
-                parameters['absolute_change_end'] = float(self.absolute_change_end_edit.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Absolute Change End.  Please fix.'
-        elif parameters['threshold_type'] == 'Percent Change':
-            try:
-                parameters['percent_change_start'] = float(self.percentage_change_start_edit.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Percent Change Start text box.  Please fix.'
-            try:
-                parameters['percent_change_end'] = float(self.percentage_change_end_edit.text())
-            except ValueError:
-                parameters['error'] = 'Could not read float from Percent Change End text box.  Please fix.'
-
-        return parameters
-
     def clean_threads(self):
         for w in self.thread_pool:
             w.cancel()
@@ -417,8 +243,8 @@ The current namespace should include:
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    pixmap = QtGui.QPixmap('splash.png')
-    splash = QtGui.QSplashScreen(pixmap, QtCore.Qt.WindowStaysOnTopHint)
+    pix_map = QtGui.QPixmap('splash.png')
+    splash = QtGui.QSplashScreen(pix_map, QtCore.Qt.WindowStaysOnTopHint)
     splash.show()
     _long_imports(splash=splash, app=app)
     splash.showMessage("Creating main window...", alignment=QtCore.Qt.AlignBottom)
@@ -435,5 +261,3 @@ if __name__ == '__main__':
 else:
     # If we are running from a tests, name != main, and we'll need to import the long imports now.
     _long_imports()
-
-
