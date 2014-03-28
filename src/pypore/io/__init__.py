30e57f6ac588fa3927135579f53fcbb50b4ee2cc
Will <wmparkin@gmail.com>
2014-03-28 13:34:29 -0400

    get DataFileReader for .h5 files

--- src/pypore/io/__init__.py
+++ src/pypore/io/__init__.py
@@ -5,13 +5,22 @@ def get_reader_from_filename(filename):
 
     # if '.h5' in filename:
     #     return open_pypore_file(filename, decimate)
-    if '.log' in filename:
-        from chimera_reader import ChimeraReader
-        try:
-            reader = ChimeraReader(filename)
-            return reader
-        except Exception, e:
-            print e
+    if '.h5' in filename:
+        from data_file_reader import DataFileReader as ReaderClass
+    elif '.log' in filename:
+        from chimera_reader import ChimeraReader as ReaderClass
+    else:
+        print "error"
+        return "No default reader available for file extension of {0}".format(filename)
+
+    try:
+        reader = ReaderClass(filename)
+        return reader
+    except Exception, e:
+        print e
+        return e.message
+
+
 
     # elif '.hkd' in filename:
     #     return open_heka_file(filename, decimate)
