a9f70de4c18d2b5cd317b74966c75b0e25f16e85
Will <wmparkin@gmail.com>
2014-03-30 16:15:53 -0400

    decouple the variance calculation from the baseline

--- src/pypore/strategies/adaptive_baseline_strategy.pyx
+++ src/pypore/strategies/adaptive_baseline_strategy.pyx
@@ -11,10 +11,13 @@ ctypedef np.float_t DTYPE_t
 cdef class AdaptiveBaselineStrategy(BaselineStrategy):
     cdef public double baseline_filter_parameter
     cdef public double variance_filter_parameter
+    # Keep track of baseline separately for the variance calculation.
+    cdef public double variance_baseline
 
     def __init__(self, double baseline=0.0, double variance=0.0, double baseline_filter_parameter=0.93,
                  double variance_filter_parameter=0.99):
         super(AdaptiveBaselineStrategy, self).__init__(baseline, variance)
+        self.variance_baseline = baseline
         self.variance = variance
         self.baseline_filter_parameter = baseline_filter_parameter
         self.variance_filter_parameter = variance_filter_parameter
@@ -25,8 +28,10 @@ cdef class AdaptiveBaselineStrategy(BaselineStrategy):
         return self.baseline
 
     cdef double compute_variance_c(self, double data_point):
+        self.variance_baseline = self.variance_filter_parameter * self.variance_baseline + (
+            1 - self.variance_filter_parameter) * data_point
         self.variance = self.variance_filter_parameter * self.variance + (1 - self.variance_filter_parameter) * pow(
-            data_point - self.baseline, 2)
+            data_point - self.variance_baseline, 2)
         return self.variance
 
     cdef double get_baseline_c(self):
@@ -38,3 +43,4 @@ cdef class AdaptiveBaselineStrategy(BaselineStrategy):
     cdef void initialize_c(self, np.ndarray[DTYPE_t] initialization_points):
         self.baseline = np.mean(initialization_points)
         self.variance = np.var(initialization_points)
+        self.variance_baseline = self.baseline
