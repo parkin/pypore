f0407aba29d0f7c19ab0b60b5bc6b8d0cd044b4f
Will <wmparkin@gmail.com>
2014-04-16 18:02:49 -0400

    allow passing in multiple files to test the scalings

--- src/pypore/i_o/tests/reader_tests.py
+++ src/pypore/i_o/tests/reader_tests.py
@@ -16,41 +16,56 @@ class ReaderTests(object):
 
     #. :py:func:`help_scaling`
     #. :py:func:`help_scaling_decimated`
+    #. :py:func:`help_get_all_data_returns_to_beginning`
 
     """
     reader_class = None
+
+    def help_get_all_data_returns_to_beginning(self):
+        """
+        Should be overridden by a subclass and return the following.
+
+        :returns: Filename
+        """
+        raise NotImplementedError('Inheritors should override this method.')
 
     def test_scaling(self):
         """
         Tests that the data is scaled correctly, from a known test file.
         """
-        filename, mean_should_be, std_dev_should_be = self.help_scaling()
+        file_names, means_should_be, std_devs_should_be = self.help_scaling()
 
-        reader = self.reader_class(filename)
+        for i, filename in enumerate(file_names):
+            mean_should_be = means_should_be[i]
+            std_dev_should_be = std_devs_should_be[i]
 
-        data = reader.get_all_data()[0]
-        reader.close()
+            reader = self.reader_class(filename)
 
-        mean = np.mean(data)
+            data = reader.get_all_data()[0]
+            reader.close()
 
-        mean_diff = abs((mean - mean_should_be) / mean_should_be)
-        self.assertLessEqual(mean_diff, 0.05,
-                             "Data mean scaled wrong. Should be {0}, got {1}.".format(mean_should_be, mean))
+            mean = np.mean(data)
 
-        std_dev = np.std(data)
+            mean_diff = abs((mean - mean_should_be) / mean_should_be)
+            self.assertLessEqual(mean_diff, 0.05,
+                                 "Data mean in '{0}' scaled wrong. "
+                                 "Should be {1}, got {2}.".format(filename, mean_should_be, mean))
 
-        std_diff = abs((std_dev - std_dev_should_be) / std_dev_should_be)
-        self.assertLessEqual(std_diff, 0.05,
-                             "Baseline scaled wrong. Should be {0}, got {1}.".format(std_dev_should_be, std_dev))
+            std_dev = np.std(data)
+
+            std_diff = abs((std_dev - std_dev_should_be) / std_dev_should_be)
+            self.assertLessEqual(std_diff, 0.05,
+                                 "Baseline in '{0}' scaled wrong. "
+                                 "Should be {1}, got {2}.".format(filename, std_dev_should_be, std_dev))
 
     def help_scaling(self):
         """
         Subclasses should override this method and return the following, in the following order.
         :returns: The following, in order:
 
-        #. filename of the test file.
-        #. known mean of the data in the test file to check against the reader_class.
-        #. known standard deviation of the data to check against the reader_class.
+        #. list of file names of the test files.
+        #. list of known means of the data in the test files to check against the reader_class.
+        #. list of known standard deviations of the data to check against the reader_class.
 
         """
         raise NotImplementedError('Inheritors should override this method.')
@@ -59,20 +74,22 @@ class ReaderTests(object):
         """
         Tests that the decimated data is scaled the same as the un-decimated data.
         """
-        filename = self.help_scaling_decimated()
+        file_names = self.help_scaling_decimated()
 
-        reader = self.reader_class(filename)
+        for filename in file_names:
+            reader = self.reader_class(filename)
 
-        data = reader.get_all_data()[0]
-        data_decimated = reader.get_all_data(decimate=True)[0]
-        reader.close()
+            data = reader.get_all_data()[0]
+            data_decimated = reader.get_all_data(decimate=True)[0]
+            reader.close()
 
-        mean = np.mean(data)
-        mean_decimated = np.mean(data_decimated)
+            mean = np.mean(data)
+            mean_decimated = np.mean(data_decimated)
 
-        mean_percent_diff = abs((mean - mean_decimated) / mean)
-        self.assertLessEqual(mean_percent_diff, 0.05,
-                             "Decimated mean scaled wrong. Should be {0}, got {1}.".format(mean, mean_decimated))
+            mean_percent_diff = abs((mean - mean_decimated) / mean)
+            self.assertLessEqual(mean_percent_diff, 0.05,
+                                 "Decimated mean in '{0}' scaled wrong. "
+                                 "Should be {1}, got {2}.".format(filename, mean, mean_decimated))
 
     def help_scaling_decimated(self):
         """
