9f95b453d4658a1a5812bd3cec9032201da3addc
Will <wmparkin@gmail.com>
2014-04-16 16:23:05 -0400

    reformatted. added some comments

--- src/pypore/i_o/tests/reader_tests.py
+++ src/pypore/i_o/tests/reader_tests.py
@@ -7,25 +7,26 @@ class ReaderTests(object):
 
     Subclasses **must** use multi-inheritance and also inherit from :py:class:`unittest.TestCase`.
 
-    The subclass **must** have a field 'READER' that is a subclass of
+    The subclass **must** have a field 'reader_class' that is a subclass of
     :py:class:`AbstractReader <pypore.i_o.abstract_reader.AbstractReader>`. For example::
 
-        self.READER = ChimeraReader
+        self.reader_class = ChimeraReader
 
     The subclasses also **must** override the following methods:
 
-    #. :py:func:`reader_test_scaling_help`
+    #. :py:func:`help_test_scaling`
+    #. :py:func:`help_test_scaling_decimated`
 
     """
-    READER = None
+    reader_class = None
 
     def test_scaling(self):
         """
         Tests that the data is scaled correctly, from a known test file.
         """
-        filename, mean_should_be, mean_places, std_dev_should_be, std_dev_places = self.reader_test_scaling_help()
+        filename, mean_should_be, mean_places, std_dev_should_be, std_dev_places = self.help_test_scaling()
 
-        reader = self.READER(filename)
+        reader = self.reader_class(filename)
 
         data = reader.get_all_data()[0]
         reader.close()
@@ -40,15 +41,15 @@ class ReaderTests(object):
         self.assertAlmostEqual(std_dev, std_dev_should_be, std_dev_places,
                                "Baseline scaled wrong. Should be {0}, got {1}.".format(std_dev_should_be, std_dev))
 
-    def reader_test_scaling_help(self):
+    def help_test_scaling(self):
         """
         Subclasses should override this method and return the following, in the following order.
         :returns: The following, in order:
 
         #. filename of the test file.
-        #. known mean of the data in the test file to check against the READER.
+        #. known mean of the data in the test file to check against the reader_class.
         #. number of decimal places to compare the mean.
-        #. known standard deviation of the data to check against the READER.
+        #. known standard deviation of the data to check against the reader_class.
         #. number of decimal places to compare the standard deviation.
 
         """
@@ -58,9 +59,9 @@ class ReaderTests(object):
         """
         Tests that the decimated data is scaled the same as the un-decimated data.
         """
-        filename = self.reader_test_scaling_decimated_help()
+        filename = self.help_test_scaling_decimated()
 
-        reader = self.READER(filename)
+        reader = self.reader_class(filename)
 
         data = reader.get_all_data()[0]
         data_decimated = reader.get_all_data(decimate=True)[0]
@@ -73,7 +74,7 @@ class ReaderTests(object):
         self.assertLessEqual(mean_percent_diff, 0.05,
                              "Decimated mean scaled wrong. Should be {0}, got {1}.".format(mean, mean_decimated))
 
-    def reader_test_scaling_decimated_help(self):
+    def help_test_scaling_decimated(self):
         """
         Subclasses should override this method and return the filename used to check that the decimated and un-decimated
         data :py:func:`get_all_data <pypore.i_o.abstract_reader.AbstractReader.get_all_data>`.
--- src/pypore/i_o/tests/test_chimera_reader.py
+++ src/pypore/i_o/tests/test_chimera_reader.py
@@ -1,28 +1,20 @@
 """
-Created on Aug 19, 2013
-
 @author: `@parkin`_
 """
 import unittest
-import os.path
-import numpy as np
+
 from pypore.i_o.chimera_reader import ChimeraReader
 from pypore.i_o.tests.reader_tests import ReaderTests
-
 import pypore.sampledata.testing_files as tf
 
 
 class TestChimeraReader(unittest.TestCase, ReaderTests):
-
-    READER = ChimeraReader
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+    reader_class = ChimeraReader
 
     def test_constructor_no_mat_spec(self):
+        """
+        Tests that an IOError is raised when no .mat spec file is next to the .log file.
+        """
         test_no_mat_chimera_files = tf.get_abs_path('chimera_empty.log')
         for filename in test_no_mat_chimera_files:
             self.assertRaises(IOError, ChimeraReader, filename)
@@ -42,26 +34,17 @@ class TestChimeraReader(unittest.TestCase, ReaderTests):
         self.assertAlmostEqual(data[0], 17.45518, 3)
         self.assertAlmostEqual(data[9], 18.0743, 3)
 
-    def test_open_chimera_data(self):
-        # Make sure path to chimera file is correct.
-        filename = tf.get_abs_path('chimera_small.log')
-        chimera_reader = ChimeraReader(filename)
-        data = chimera_reader.get_all_data(False)
-        self._test_small_chimera_file_help(data)
-        chimera_reader.close()
-
-    def reader_test_scaling_help(self):
+    def help_test_scaling(self):
         filename = tf.get_abs_path('spheres_20140114_154938_beginning.log')
         mean_should_be = 7.57604  # Value gotten from original MATLAB script
         std_should_be = 1.15445  # Value gotten from original MATLAB script
         return filename, mean_should_be, 2, std_should_be, 2
 
-    def reader_test_scaling_decimated_help(self):
+    def help_test_scaling_decimated(self):
         filename = tf.get_abs_path('spheres_20140114_154938_beginning.log')
         return filename
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
 
